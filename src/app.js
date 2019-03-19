0a940267efda6c268eafd590bd31fc8ed030d604
Semih Arslan <semihsemih@me.com>
2019-03-19 15:55:26 +0300

    Template and 404 pages

--- src/app.js
+++ src/app.js
@@ -1,10 +1,20 @@
 const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
-const publicDirectoryPath = path.join(__dirname, '../public')
 
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static sirectory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
@@ -24,6 +34,7 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
   res.render('help', {
     title: 'Help Page',
+    name: 'Semih Arslan',
     helpText: 'Please help me!'
   })
 })
@@ -32,6 +43,22 @@ app.get('/weather', (req, res) => {
   res.send({
     forecast: 'It is snowing',
     location: 'Ankara'
+  })
+})
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Semih Arslan',
+    errorMessage: 'Help article not found.'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Semih Arslan',
+    errorMessage: 'Page not found.'
   })
 })
 
