a0138b60ee928f5c98149f9157f550e11d045437
Semih Arslan <semihsemih@me.com>
2019-03-19 14:02:41 +0300

    Dynamic pages created with hadlebars templating

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Semih Arslan'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About me',
+    name: 'Semih Arslan'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    helpText: 'Please help me!'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
