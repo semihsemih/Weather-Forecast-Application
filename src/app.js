66759f46ea4ff48297290b33d314faff6800bf66
Semih Arslan <semihsemih@me.com>
2019-03-19 13:15:26 +0300

    Serving up static assets

--- src/app.js
+++ src/app.js
@@ -1,21 +1,16 @@
+const path = require('path')
 const express = require('express')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send('Hello express!')
-})
-
-app.get('/help', (req, res) => {
-  res.send('Help page')
-})
-
-app.get('/about', (req, res) => {
-  res.send('About page')
-})
+app.use(express.static(publicDirectoryPath))
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather')
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Ankara'
+  })
 })
 
 app.listen(3000, () => {
