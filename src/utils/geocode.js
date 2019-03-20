22bcb9696b3b2f1a4c3a7bf93e64bd6d385f3573
Semih Arslan <semihsemih@me.com>
2019-03-20 12:43:51 +0300

    Bug fixed in geocode.js

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -3,7 +3,7 @@ const request = require('request')
 geocode = (address, callback) => {
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2VtaWhhcnNsYW4iLCJhIjoiY2p0ZTlkcGVuMDJ0dzQ0cGs3cTZiM3kzZiJ9.r9zNSx7l4oI1t935nWED5w'
 
-  request({ url, json: true}, (error, { body }) => {
+  request({ url, json: true}, (error, { body } = {}) => {
     if (error) {
       callback('Unable to connect to location services!', undefined)
     } else if (body.features.length === 0) {
