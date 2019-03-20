002cfba176cd044c75027652b63299a585b7748c
Semih Arslan <semihsemih@me.com>
2019-03-20 11:22:34 +0300

    Creating search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,14 @@
 console.log('Client side javascript file is loaded!')
 
-fetch('http://localhost:3000/weather?address=Boston').then((response) => {
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
   response.json().then((data) => {
     if (data.error) {
       console.log(data.error)
@@ -10,4 +18,7 @@ fetch('http://localhost:3000/weather?address=Boston').then((response) => {
 
     }
   })
+})
+  
+  console.log(location)
 })
