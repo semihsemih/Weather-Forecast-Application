fc7029327c3a8fded45b9448a6b741a8f3983ec4
Semih Arslan <semihsemih@me.com>
2019-03-20 15:54:20 +0300

    Adding high and low temperature in daily data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ forecast = (latitude, longtitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location')
     } else {
-      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+      callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
     }
   })
 }
